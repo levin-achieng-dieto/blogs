e7962dddcddd0ba8a85a660dfba31c994f3cf206
Dennis <dennisgachomo@gmail.com>
2022-11-09 18:11:38 +0300

    post added

--- blogs-client/src/components/App.js
+++ blogs-client/src/components/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import Footer from './Footer'
+import { useEffect, useState } from "react";
+import Header from "./Header";
+import Post from "./Post";
+import axios from "axios";
+import Footer from "./Footer";
 
-function App(){
-    return (
-        <>
-            < Footer/>
-        </>
-    )
+const api = "http://localhost:9292/posts";
+
+function App() {
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  const loadPosts = async () => {
+    const response = await axios.get(api);
+    setPost(response.data);
+  };
+
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Post posts={post} loadPosts={loadPosts} />
+      <Footer/>
+    </div>
+  );
 }
-export default App
+
+export default App;
--- blogs-client/src/index.js
+++ blogs-client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+// import $ from "jquery";
+// import Popper from "popper.js";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '../src/components/App';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,8 +13,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//
