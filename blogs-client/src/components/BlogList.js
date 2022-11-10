ab609fb3a93f410adb5c4bf244622866b5492a12
kevin-lomenen <kevin.lomenen@student.moringaschool.com>
2022-11-10 12:47:54 +0300

    card updated

--- blogs-client/src/components/BlogList.js
+++ blogs-client/src/components/BlogList.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Card from "./Card";
+// import React from "react";
+// import Card from "./Card";
 
 
-function BlogList(){
-    return(
-        <>
-            <h1>BlogList</h1>
-        </>
-    )
-}
+// function BlogList(){
+//     return(
+//         <>
+//             <h1>BlogList</h1>
+//         </>
+//     )
+// }
 
-export default BlogList
+// export default BlogList
